b8514755c2d34f51213c8b8709bd8d5aad3e950a
dduckmanton <daryl.duckmanton@racingaustralia.horse>
2020-02-13 16:12:33 +1100

    Some responsive web design changes. Will refine later.

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,7 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss']
 })
-export class SplashComponent { }
+export class SplashComponent implements OnInit, OnDestroy { 
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private routerService: Router
+  ) { }
+
+  ngOnInit() {
+    const mediaQuery = '(min-width: 768px) and (orientation: portrait), (min-width: 824px) and (orientation: landscape)';
+    this.breakpointObserver.observe(mediaQuery).subscribe(
+      observer => {
+        if (observer.matches) {
+          this.navigateToSearchPage();
+        }
+      }
+    )
+  }
+
+  navigateToSearchPage() {
+    this.routerService.navigateByUrl('/search');
+  }
+
+  ngOnDestroy() {
+
+  }
+}
