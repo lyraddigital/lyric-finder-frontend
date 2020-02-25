01eb4759d0d1fadf482bacf0eb6636127ca6fa43
Daryl Duckmanton <daryl@lyraddigital.com>
2020-02-25 20:31:38 +1100

    Started investigating the routing issue.

--- src/app/songs/search/search.component.ts
+++ src/app/songs/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { SearchService } from './search.service';
@@ -13,10 +14,18 @@ export class SearchComponent {
   searchResults$: Observable<Array<SearchResultItem>>;
   emptySearch = true;
 
-  constructor(private searchService: SearchService) { }
+  constructor(
+    private searchService: SearchService,
+    private router: Router
+  ) { }
 
   performSearch(searchTerm: string) {
     this.emptySearch = !searchTerm || searchTerm.length === 0;
+
+    if (!this.emptySearch) {
+      this.router.navigateByUrl(`/search/${searchTerm}`);
+    }
+
     this.searchResults$ = this.searchService.getSearchResults(searchTerm);
   }
 }
