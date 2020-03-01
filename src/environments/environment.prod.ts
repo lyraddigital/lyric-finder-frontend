96aacfc52c2e5f29ec5c56b5cc594825e5caa970
Daryl Duckmanton <daryl@lyraddigital.com>
2020-03-02 02:52:13 +1100

    prod build changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  deezerRapidApiDomainName: 'deezerdevs-deezer.p.rapidapi.com',
+  deezerRapidApiKey: '5bbc7ff12amsh9cc0cf2626ff24ep157130jsn49b474194ca6'
 };
